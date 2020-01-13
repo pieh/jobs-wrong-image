f7e5f903ac04c11568dc04bf62eaa3c565ee2ffe
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-01-13 16:19:24 +0100

    repro

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,18 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const path = require(`path`)
+
 module.exports = {
   /* Your site config here */
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.resolve(`./src/images`),
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  const data = useStaticQuery(graphql`
+    {
+      imageSharp {
+        resize(width: 1500, height: 1500) {
+          src
+          width
+          height
+        }
+      }
+    }
+  `)
+
+  return (
+    <div>
+      Hello world!
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <img src={data.imageSharp.resize.src} />
+    </div>
+  )
+}
